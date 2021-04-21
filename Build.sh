b6a5cafdbd3432190f2b782ca3a4df7da09afb90
johncecilio <40663480+johncecilio@users.noreply.github.com>
2021-04-21 08:03:45 +0800

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -2,8 +2,8 @@
 #
 
 docker build -t mymicrosvc:v1 -f Dockerfile .
-docker tag mymicrosvc:v1 $(registry)/$(username)/mymicrosvc:v1
+docker tag mymicrosvc:v1 $(env.registry)/$(env.username)/mymicrosvc:v1
 oc login -u $(username) -p $(password)
 oc project myproject
-docker login -u $(username) -p $(oc whoami -t) $(registry)
-docker push $(registry)/$(username)/mymicrosvc:v1
+docker login -u $(env.username) -p $(oc whoami -t) $(env.registry)
+docker push $(env.registry)/$(env.username)/mymicrosvc:v1
